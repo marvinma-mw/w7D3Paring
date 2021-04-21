3a6e0afa9d044984a6d7c62e66d3fa575919965b
Taylor Hogan <taylorb.hogan@gmail.com>
2021-04-21 15:17:45 -0700

    Solve 2.2

--- big-o-intro-project/02-anagrams.js
+++ big-o-intro-project/02-anagrams.js
@@ -1,33 +1,55 @@
-// function firstAnagram(str1, str2) {
-//   // Code goes here ....
-//   let arr1 = str1.split("");
-//   let arr2 = str2.split("");
-//   for(let i=0; i<arr1.length;i++){
-//     console.log("arr1: ",arr1[i])
-//     console.log("arr2: ",arr2.indexOf(arr1[i]))
-//    arr2.splice(arr2.indexOf(arr1[i]),1)
-//    console.log(arr2)
-//   }
-//   if(!arr2.length){
-//     return true;
-//   }
-// }
-
 function firstAnagram(str1, str2) {
-  for(let i=0;i<str1.length;i++){
-    str2.replace(str2.indexOf(str1[i]),"")
+  let arr1 = str1.split("");
+  let arr2 = str2.split("");
+  for(let i=0; i<arr1.length;i++){
+   if (arr2.indexOf(arr1[i]) !== -1){
+     arr2.splice(arr2.indexOf(arr1[i]),1)
+   }
   }
-    if(!str2.length){
+  if(!arr2.length){
     return true;
+  } else {
+    return false;
   }
-}0
+}
 
-console.log(firstAnagram("gizmo", "sally"));
-// console.log(firstAnagram("elvis", "lives"));
+// console.log(firstAnagram("gizmo", "sally")); //false
+// console.log(firstAnagram("elvis", "lives")); //true
+
+
+
+
+
 
 function secondAnagram(str1, str2) {
-  // Code goes here ....
+  let newStr1 = str1.split("").sort().join("")
+  let newStr2 = str2.split("").sort().join("")
+
+  if (newStr1 === newStr2){
+    return true;
+  } else {
+    return false;
+  }
 }
+console.log(secondAnagram("gizmo", "sally")); //false
+console.log(secondAnagram("elvis", "lives")); //true
+
+
+
+
+/*Write a method secondAnagram that solves the problem by
+sorting both strings alphabetically.
+The strings are then anagrams if and only if
+the sorted versions are the identical.*/
+
+
+
+
+
+
+
+
+
 
 
 function thirdAnagram(str1, str2) {
