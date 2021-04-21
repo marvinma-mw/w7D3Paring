325db91e5df2008d5dfca53de27053bd3df516e5
marvin <marvinma.mw@gmail.com>
2021-04-21 18:38:17 -0400

    question 2 completed

--- big-o-intro-project/02-anagrams.js
+++ big-o-intro-project/02-anagrams.js
@@ -31,8 +31,8 @@ function secondAnagram(str1, str2) {
     return false;
   }
 }
-console.log(secondAnagram("gizmo", "sally")); //false
-console.log(secondAnagram("elvis", "lives")); //true
+// console.log(secondAnagram("gizmo", "sally")); //false
+// console.log(secondAnagram("elvis", "lives")); //true
 
 
 
@@ -46,17 +46,47 @@ the sorted versions are the identical.*/
 
 
 
-
-
-
-
-
-
 function thirdAnagram(str1, str2) {
-  // Code goes here ....
+  let arr1 = str1.split("");
+  let arr2 = str2.split("");
+  let obj1 = {};
+  let obj2 = {};
+  for(let i=0;i<arr1.length;i++){
+    let char = arr1[i];
+    if(obj1[char]===undefined){
+      obj1[char]=1;
+    }else{
+      obj1[char]+=1;
+    }
+  }
+  // console.log(obj1);
+  for(let i=0;i<arr2.length;i++){
+    let char = arr2[i];
+    if(obj2[char]===undefined){
+      obj2[char]=1;
+    }else{
+      obj2[char]+=1;
+    }
+  }
+  // console.log(obj2)
+  for(key in obj1){
+    if(obj1[key]!==obj2[key]){
+      return false;
+    }
+  }
+  // if(obj1===obj2){
+  //   return true;
+  // }else{
+  //   return false;
+  // }
+return true;
 }
 
+console.log(thirdAnagram("gizmo", "sally")); //false
+console.log(thirdAnagram("elvis", "lives")); //true
 
 function fourthAnagram(str1, str2) {
   // Code goes here ....
 }
+
+
